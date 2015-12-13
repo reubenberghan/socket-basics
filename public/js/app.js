3534f20afd94459c7faaa16791616d19c17a32ba
Reuben Berghan <reuben.berghan@gmail.com>
2015-12-14 10:26:39 +1100

    Added custome message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on('connect', function() {
 	$('p.connection-message').html('Connected to socket.io server!');
 });
+
+socket.on('message', function(message) {
+	$('div.message-container').html('New message: ' + message.text);
+});
--- server.js
+++ server.js
@@ -6,8 +6,18 @@ var PORT = process.env.PORT || 3000,
 	
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function() {
+io.on('connection', function(socket) {
 	console.log('User connected via socket.io!');
+	
+	socket.on('message', function(message) {
+		console.log('Message recieved: ' + message.text);
+		
+		socket.broadcast.emit('message', message);
+	});
+	
+	socket.emit('message', {
+		text: 'Welcome to the chat application'
+	});
 });
 
 http.listen(PORT, function() { console.log('Server started on port', PORT); })
